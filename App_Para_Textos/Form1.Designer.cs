b2404474fef0c033bbe7ef044bd0184de4c5ea90
DIEGO <DIEGO@DESKTOP-OMG42QK>
2022-03-03 12:47:18 -0600

    Agregar archivos de proyecto.

---
+++ App_Para_Textos/Form1.Designer.cs
@@ -0,0 +1,159 @@
+﻿
+namespace App_Para_Textos
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.LBNom = new System.Windows.Forms.Label();
+            this.LbAp = new System.Windows.Forms.Label();
+            this.txtNombre = new System.Windows.Forms.TextBox();
+            this.txtApellido = new System.Windows.Forms.TextBox();
+            this.Rtxt1 = new System.Windows.Forms.RichTextBox();
+            this.btnEjecutar = new System.Windows.Forms.Button();
+            this.btnGuardar = new System.Windows.Forms.Button();
+            this.btnRefresh = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // LBNom
+            // 
+            this.LBNom.AutoSize = true;
+            this.LBNom.Location = new System.Drawing.Point(80, 89);
+            this.LBNom.Name = "LBNom";
+            this.LBNom.Size = new System.Drawing.Size(44, 13);
+            this.LBNom.TabIndex = 0;
+            this.LBNom.Text = "Nombre";
+            // 
+            // LbAp
+            // 
+            this.LbAp.AutoSize = true;
+            this.LbAp.Location = new System.Drawing.Point(80, 138);
+            this.LbAp.Name = "LbAp";
+            this.LbAp.Size = new System.Drawing.Size(49, 13);
+            this.LbAp.TabIndex = 1;
+            this.LbAp.Text = "Apellidos";
+            // 
+            // txtNombre
+            // 
+            this.txtNombre.Location = new System.Drawing.Point(130, 89);
+            this.txtNombre.Name = "txtNombre";
+            this.txtNombre.Size = new System.Drawing.Size(107, 20);
+            this.txtNombre.TabIndex = 2;
+            // 
+            // txtApellido
+            // 
+            this.txtApellido.Location = new System.Drawing.Point(137, 138);
+            this.txtApellido.Name = "txtApellido";
+            this.txtApellido.Size = new System.Drawing.Size(100, 20);
+            this.txtApellido.TabIndex = 3;
+            // 
+            // Rtxt1
+            // 
+            this.Rtxt1.HideSelection = false;
+            this.Rtxt1.Location = new System.Drawing.Point(288, 12);
+            this.Rtxt1.Name = "Rtxt1";
+            this.Rtxt1.Size = new System.Drawing.Size(569, 519);
+            this.Rtxt1.TabIndex = 4;
+            this.Rtxt1.Text = "";
+            this.Rtxt1.TextChanged += new System.EventHandler(this.Rtxt1_TextChanged);
+            // 
+            // btnEjecutar
+            // 
+            this.btnEjecutar.Location = new System.Drawing.Point(34, 183);
+            this.btnEjecutar.Name = "btnEjecutar";
+            this.btnEjecutar.Size = new System.Drawing.Size(90, 68);
+            this.btnEjecutar.TabIndex = 5;
+            this.btnEjecutar.Text = "Ejecutar";
+            this.btnEjecutar.UseVisualStyleBackColor = true;
+            this.btnEjecutar.Click += new System.EventHandler(this.btnEjecutar_Click);
+            // 
+            // btnGuardar
+            // 
+            this.btnGuardar.Location = new System.Drawing.Point(176, 183);
+            this.btnGuardar.Name = "btnGuardar";
+            this.btnGuardar.Size = new System.Drawing.Size(90, 68);
+            this.btnGuardar.TabIndex = 6;
+            this.btnGuardar.Text = "Guardar";
+            this.btnGuardar.UseVisualStyleBackColor = true;
+            this.btnGuardar.Click += new System.EventHandler(this.btnGuardar_Click);
+            // 
+            // btnRefresh
+            // 
+            this.btnRefresh.Location = new System.Drawing.Point(34, 298);
+            this.btnRefresh.Name = "btnRefresh";
+            this.btnRefresh.Size = new System.Drawing.Size(90, 68);
+            this.btnRefresh.TabIndex = 7;
+            this.btnRefresh.Text = "Borrar";
+            this.btnRefresh.UseVisualStyleBackColor = true;
+            this.btnRefresh.Click += new System.EventHandler(this.btnRefresh_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(176, 298);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(90, 68);
+            this.button1.TabIndex = 8;
+            this.button1.Text = "Abrir";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(907, 580);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.btnRefresh);
+            this.Controls.Add(this.btnGuardar);
+            this.Controls.Add(this.btnEjecutar);
+            this.Controls.Add(this.Rtxt1);
+            this.Controls.Add(this.txtApellido);
+            this.Controls.Add(this.txtNombre);
+            this.Controls.Add(this.LbAp);
+            this.Controls.Add(this.LBNom);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label LBNom;
+        private System.Windows.Forms.Label LbAp;
+        private System.Windows.Forms.TextBox txtNombre;
+        private System.Windows.Forms.TextBox txtApellido;
+        private System.Windows.Forms.RichTextBox Rtxt1;
+        private System.Windows.Forms.Button btnEjecutar;
+        private System.Windows.Forms.Button btnGuardar;
+        private System.Windows.Forms.Button btnRefresh;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ App_Para_Textos/Form1.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Diagnostics;
+using System.Threading.Tasks;
+using System.IO;
+
+using System.Windows.Forms;
+
+namespace App_Para_Textos
+{
+    public partial class Form1 : Form
+    {
+        Process Proceso = new Process();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnGuardar_Click(object sender, EventArgs e)
+        {
+            StreamWriter escribir = new StreamWriter(@"E:\Nueva carpeta (2)\Archivo.txt", true);
+            try
+            {
+                escribir.WriteLine(txtNombre.Text );
+                escribir.WriteLine( txtApellido.Text);
+                escribir.WriteLine("\n");
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Error");
+               
+            }
+            escribir.Close();
+
+        }
+
+        private void btnEjecutar_Click(object sender, EventArgs e)
+        {
+            StreamReader Leer = new StreamReader(@"E:\Nueva carpeta (2)\Archivo.txt", true);
+            String Linea;
+
+            try
+            {
+                Linea = Leer.ReadLine();
+                while (Linea != null) 
+                {
+                    Rtxt1.AppendText(Linea + "\n");
+                    Linea = Leer.ReadLine();
+                } 
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Error");
+
+            }
+            Leer.Close();
+
+
+
+        }
+
+        private void Form_Activate()
+        {
+            txtApellido.Clear();
+            txtNombre.Clear();
+            Rtxt1.Clear();
+        }
+        private void Abrir_Word()
+        {
+            OpenFileDialog abrir  = new OpenFileDialog();
+        
+            if (abrir.ShowDialog()==DialogResult.OK)
+            {
+                string Direccion = abrir.FileName;
+                Proceso.StartInfo.FileName = Direccion;
+                Proceso.Start();
+                
+            }
+
+        }
+        private void btnOpenFile_Click(object sender, System.EventArgs e)
+        {
+           
+        }
+
+        private  void btnRefresh_Click(object sender, EventArgs e)
+        {
+            Form_Activate();
+            Abrir_Word();
+
+            SaveFileDialog GUARDAR = new SaveFileDialog();
+            GUARDAR.Filter = "Documento de texto|*.docx*";
+            GUARDAR.Title = "Guardar RichTextBox";
+            GUARDAR.FileName = "Sin Titulo 1 ";
+            var resultado = GUARDAR.ShowDialog();
+            if (resultado==DialogResult.OK)
+            {
+                StreamWriter escribir = new StreamWriter(GUARDAR.FileName);
+                foreach (object line in Rtxt1.Lines)
+                {
+                    escribir.WriteLine(line);
+                }
+                escribir.Close();
+            }
+
+            //OpenFileDialog nuevo = new OpenFileDialog();
+            //if (nuevo.ShowDialog() == DialogResult.OK)
+            //{
+            //    Rtxt1.LoadFile(nuevo.FileName, RichTextBoxStreamType.RichText);
+            //}
+
+
+        }
+
+        private void Rtxt1_TextChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog ABRIR = new OpenFileDialog();
+            ABRIR.Filter = "Documento de texto|*.docx*";
+            ABRIR.Title = "Guardar RichTextBox";
+            ABRIR.FileName = "Sin Titulo 1 ";
+            var resultado = ABRIR.ShowDialog();
+            if (resultado == DialogResult.OK)
+            {
+                StreamReader Leer = new StreamReader(ABRIR.FileName);
+                Rtxt1.Text = Leer.ReadToEnd();
+                Leer.Close();
+            }
+
+        }
+    }
+}
